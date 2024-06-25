056aa6bb0479440a3263a3d224b127fc22a32125
John Nguyen <johnv.nguyen14@gmail.com>
2024-06-25 06:06:59 -0400

    Spruced up the homepage A LOT. Added a lot of styling and added blocks of information to the homepage.

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -19,15 +19,34 @@ function Homepage() {
                     <Header title={'Honkai: Star Rail Wiki'} />
                     <div className='home-info'>
                         <div id='content'>
+
                             <div id='header'>
                                 <h1>Home</h1>
                             </div>
+
                             <div id='welcome'>
                                 <img src={hsr_banner} alt="welcome-banner" />
-                                <div className='welcome-msg'>
-                                    <h2 >Welcome to the Honkai: Star Rail Wiki!</h2>
-                                    <h5>Official Links:</h5>
+                                <div id='links'>
+                                    <h3>Welcome to the Honkai: Star Rail Wiki!</h3>
+                                    <h4>Official Links:</h4>
+                                    <a href="https://hsr.hoyoverse.com/en-us/">Official Website  ✦ </a>
+                                    <a href="https://www.hoyolab.com/home">HoyoLab Forums  ✦ </a>
+                                    <a href="https://www.facebook.com/HonkaiStarRail/">Facebook  ✦ </a>
+                                    <a href="https://x.com/honkaistarrail">Twitter  ✦ </a>
+                                    <a href="https://www.youtube.com/@HonkaiStarRail">YouTube  ✦ </a>
+                                    <a href="https://www.reddit.com/r/HonkaiStarRail/">Reddit ✦ </a>
+                                    <br />
+                                    <br />
+                                    <a href="https://discord.com/invite/honkaistarrail">Discord  ✦ </a>
+                                    <a href="https://www.instagram.com/honkaistarrail/">Instagram  ✦ </a>
+                                    <a href="https://www.tiktok.com/@honkaistarrail_official">TikTok</a>
                                 </div>
+                            </div>
+
+                            <div id='about'>
+                                <h3>About the Game</h3>
+                                <p><a href="https://hsr.hoyoverse.com/en-us/">Honkai: Star Rail</a> is a turn based strategy RPG title created by the makers of Genshin Impact and Honkai Impact, Hoyoverse. Come aboard  the Astral Express and explore through the endless reaches of the universe filled with adventures and thrills.</p>
+                                <p>This wiki is an English resource for information about the Global version of the game. There are <span>unmakred spoilers</span> on this wiki.</p>
                             </div>
                         </div>
                     </div>
