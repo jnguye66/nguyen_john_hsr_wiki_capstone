03a952bc53403dc6faf98b3653b240875f6c9fc6
John Nguyen <johnv.nguyen14@gmail.com>
2024-06-26 03:39:23 -0400

    Finished adding navigation Links to Characters.jsx page. All formatting on each of the characters pages is good in fullscreen mode, but still not dynamic enough for any other sizes.

--- src/pages/characters/Characters.jsx
+++ src/pages/characters/Characters.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Components
 import Navbar from '../../components/Navbar';
@@ -22,7 +23,7 @@ function Characters() {
 
                 <div className='right'>
                     <Header title={'Characters'} />
-                    
+
                     <div className='char-info'>
                         <div id='content'>
 
@@ -30,27 +31,38 @@ function Characters() {
                                 <h1>Characters</h1>
                             </div>
 
+                            <h3 id='astral-express-header'>Astral Express <hr /></h3>
                             <div id='grid-chars'>
-                                <div id='box-char'>
-                                    <img src={tb_pfp} alt="Trailblazer" />
-                                    <p>Trailblazer</p>
-                                </div>
-                                <div id='box-char'>
-                                    <img src={m7_pfp} alt="March 7th" />
-                                    <p>March 7th</p>
-                                </div>
-                                <div id='box-char'>
-                                    <img src={dan_pfp} alt="Dan Heng" />
-                                    <p>Dan Heng</p>
-                                </div>
-                                <div id='box-char'>
-                                    <img src={himeko_pfp} alt="Himeko" />
-                                    <p>Himeko</p>
-                                </div>
-                                <div id='box-char'>
-                                    <img src={welt_pfp} alt="Welt Yang" />
-                                    <p>Welt Yang</p>
-                                </div>
+                                <Link to={'/chars/667bc2a2c026d5d119784bb6'} >
+                                    <div id='box-char'>
+                                        <img src={tb_pfp} alt="Trailblazer" />
+                                        <p>Trailblazer</p>
+                                    </div>
+                                </Link>
+                                <Link to={'/chars/667bc2a2c026d5d119784bc6'}>
+                                    <div id='box-char'>
+                                        <img src={m7_pfp} alt="March 7th" />
+                                        <p>March 7th</p>
+                                    </div>
+                                </Link>
+                                <Link to={'/chars/667bc2a2c026d5d119784bd7'}>
+                                    <div id='box-char'>
+                                        <img src={dan_pfp} alt="Dan Heng" />
+                                        <p>Dan Heng</p>
+                                    </div>
+                                </Link>
+                                <Link to={'/chars/667bc2a2c026d5d119784be8'}>
+                                    <div id='box-char'>
+                                        <img src={himeko_pfp} alt="Himeko" />
+                                        <p>Himeko</p>
+                                    </div>
+                                </Link>
+                                <Link to={'/chars/667bc2a2c026d5d119784bf9'}>
+                                    <div id='box-char'>
+                                        <img src={welt_pfp} alt="Welt Yang" />
+                                        <p>Welt Yang</p>
+                                    </div>
+                                </Link>
                             </div>
                         </div>
                     </div>
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -54,32 +54,40 @@ function Homepage() {
                                 <p><a href="https://hsr.hoyoverse.com/en-us/">Honkai: Star Rail</a> is a turn based strategy RPG title created by the makers of <a href="https://genshin.hoyoverse.com/en/">Genshin Impact</a> and <a href="https://honkaiimpact3.hoyoverse.com/global/en-us/fab">Honkai Impact</a>, Hoyoverse. Come aboard  the Astral Express and explore through the endless reaches of the universe filled with adventures and thrills.</p>
                                 <p>This wiki is an English resource for information about the Global version of the game. There are <span id='spoilers'>unmarked spoilers</span> on this wiki.</p>
                             </div>
-
+                            {/* Navigation for each character's page on Homepage */}
                             <div id='home-chars'>
                                 <Link to={'/chars'}><h3>Characters</h3></Link>
                                 <div id='grid-chars'>
-                                    <Link to={'/chars/667ba3fdaf902e0e298268cf'} style={{textDecorationColor: 'none'}}>
+                                    <Link to={'/chars/667bc2a2c026d5d119784bb6'} style={{ textDecorationColor: 'none' }}>
                                         <div id='box-char'>
                                             <img src={tb_pfp} alt="Trailblazer" />
                                             <p>Trailblazer</p>
                                         </div>
                                     </Link>
-                                    <div id='box-char'>
-                                        <img src={m7_pfp} alt="March 7th" />
-                                        <p>March 7th</p>
-                                    </div>
-                                    <div id='box-char'>
-                                        <img src={dan_pfp} alt="Dan Heng" />
-                                        <p>Dan Heng</p>
-                                    </div>
-                                    <div id='box-char'>
-                                        <img src={himeko_pfp} alt="Himeko" />
-                                        <p>Himeko</p>
-                                    </div>
-                                    <div id='box-char'>
-                                        <img src={welt_pfp} alt="Welt Yang" />
-                                        <p>Welt Yang</p>
-                                    </div>
+                                    <Link to={'/chars/667bc2a2c026d5d119784bc6'}>
+                                        <div id='box-char'>
+                                            <img src={m7_pfp} alt="March 7th" />
+                                            <p>March 7th</p>
+                                        </div>
+                                    </Link>
+                                    <Link to={'/chars/667bc2a2c026d5d119784bd7'}>
+                                        <div id='box-char'>
+                                            <img src={dan_pfp} alt="Dan Heng" />
+                                            <p>Dan Heng</p>
+                                        </div>
+                                    </Link>
+                                    <Link to={'/chars/667bc2a2c026d5d119784be8'}>
+                                        <div id='box-char'>
+                                            <img src={himeko_pfp} alt="Himeko" />
+                                            <p>Himeko</p>
+                                        </div>
+                                    </Link>
+                                    <Link to={'/chars/667bc2a2c026d5d119784bf9'}>
+                                        <div id='box-char'>
+                                            <img src={welt_pfp} alt="Welt Yang" />
+                                            <p>Welt Yang</p>
+                                        </div>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
